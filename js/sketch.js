ffd2ceff849ecbe7d81d267d436981e2d4414f8f
carmelo92 <ci2214@columbia.edu>
2017-03-30 23:52:23 -0400

    API

--- js/sketch.js
+++ js/sketch.js
@@ -4,6 +4,8 @@ var city;
 var units = 'metric';
 var weatherData; 
 var temperature = 0;
+var temp_min = 0;
+var temp_max = 0;
 var humidity = 0;
 var button;
 
@@ -22,13 +24,48 @@ function  queryAPI(){
 function getWeatherData (apiData){
 	weatherData = apiData;
 	temperature = weatherData.main.temp;
-	himidity = weatherData.main.humidity;
+	temp_max = weatherData.main.temp_max;
+	temp_min = weatherData.main.temp_min;
+	humidity = weatherData.main.humidity;
 	console.log(weatherData);
 }
 
 function draw (){
 	background (0);
-	fill(255);
+	fill(255,99,71);
 	noStroke();
-	ellipse(200,200,temperature*10,temperature*10);
+	ellipse(100,100,temp_max*10,temp_max*10);
+	fill(0,255,255)
+	ellipse(100,100,temp_min*10,temp_min*10);
+
+	if (weatherData) {
+		fill(255)
+		textSize(12)
+		text('Hey, the temperature in '+ city.value(), 200,100 );
+		text('     is', 400,100 )
+		textSize(24)
+		text(temperature, 450,100);
+		textSize(12)
+		text('Degree Celsius    | ', 500,100);
+		text('MIN', 630,100);
+		fill(0,255,255)
+		textSize(24)
+		text(temp_min, 660,100);
+		fill(255)
+		textSize(12)
+		text('         &   MAX', 670,100);
+		fill(255,99,71)
+		textSize(24)
+		text(temp_max, 750,100);
+		}
+		else { text('not working',200,200);	
+		}
+
+	if (weatherData) {
+		fill(255)
+		textSize(12)
+		text('...do not forget about the humidity, is '+ humidity + ' %', 200, 150);
+		}
+		else { text('not working',300,200);
+		}
 }
